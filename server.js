313e57ab05495197a8a26675a8250f586eeabecc
Dealan <dealan.diongzon@gmail.com>
2019-09-04 22:09:50 -0400

    added boiler plate code for server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+import { listenerCount } from "cluster";
+
+import { Mongoose } from "mongoose";
+
+const express = require('express');
+const mongoose = require('mongoose');
+const logger = require("morgan");
+
+// turn on our app
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// set up middleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.JSON());
+app.use(logger('dev'));
+
+// turn on routes
+const routes = require('./routes');
+app.use(routes);
+
+// set up mongoose connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+    useNewUrlParser: true
+});
+
+// start API server
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+})
+
