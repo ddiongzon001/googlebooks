e1f718b4e7f6cec53b85509c1d5d6048a4bac1d2
Dealan <dealan.diongzon@gmail.com>
2019-09-04 20:46:52 -0400

    added modules

---
+++ models/Book.js
@@ -0,0 +1,27 @@
+// import mongoose
+
+const mongoose = require ('mongoose');
+
+// get Schema constructor out of mongoose;
+const { Schema } = mongoose;
+
+// create schema for books
+const bookSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    authors: {
+        type: String,
+        required: true
+    },
+    description: String,
+    image: String,
+    link: String
+
+});
+
+// creates the model
+const Book = mongoose.model ("Book", bookSchema);
+
+module.exports = Book;
