9a5a45f74669a0ca3e1c3c1124c6e791879741de
Dealan <dealan.diongzon@gmail.com>
2019-09-07 16:22:21 -0400

    made stateless jumbotron

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,2 +1,35 @@
 import React from 'react';
-import { NavLink }
+import { NavLink } from 'react-router-dom'
+
+const Jumbotron = props => {
+    return (
+        <div
+            className={`
+        jumbotron
+        ${props.fluid ? jumbotron - fluid : ""}
+        bg-${props.bg || 'default'}
+        text-${props.color || 'dark'}
+        text-center 
+        `}>
+            <Container>
+                <Row helper={'justify-content-around'}>
+                    <Column>
+                        <h1>{props.pageTitle}</h1>
+                    </Column>
+                </Row>
+                <Row helper={'justify-content-around'}>
+                    <Column md={6}>
+                        <NavLink to="/search" className="btn btn-info btn-lg">
+                            Search
+                        </NavLink>
+                    </Column>
+                    <Column md={6}>
+                        <NavLink to="/save" className="btn btn-info btn-lg">
+                            Saved
+                        </NavLink>
+                    </Column>
+                </Row>
+            </Container>
+        </div>
+    )
+}
