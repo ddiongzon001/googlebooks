8631625b750ae4bff4ac9e45ca8ff034c800cd9e
Dealan <dealan.diongzon@gmail.com>
2019-09-08 00:35:08 -0400

    changeddddd routes again

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,11 @@ const apiRoutes = require("./api");
 
 router.use('/api', apiRoutes);
 
+if (process.env.NODE_ENV === "production"){
+    router.get("*", ((req, res) => {
+        res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    });
+}
+
+
 module.exports = router;
